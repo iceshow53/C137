34c033e4152801bb503f868d8e9613ff25a3b7b9
iceshow53 <iceshow53@gmail.com>
2023-03-06 13:49:32 +0900

    Unity

---
+++ Alpha/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Hor = Input.GetAxis("Horizontal");
+        float Ver = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(Hor * Time.deltaTime * Speed, Ver * Time.deltaTime * Speed, 0.0f);
+    }
+}
